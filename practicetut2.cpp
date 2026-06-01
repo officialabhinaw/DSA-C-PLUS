464f762d7723c442b4482bc92aea50e2e22f6446
Abhinaw kumar <>
2026-06-01 15:10:45 +0530

    add practice files

---
+++ practicetut2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,area;
+    cout<<"Enter a side of wall ";
+    cin>>a;
+    area=a*a;
+    cout<<"area of the square " << area <<endl;
+    return 0;
+}
---
+++ practicetut3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+  cout<<"Enter a first number a";
+  cin>>a;
+  cout<<"Enter a first number b";
+  cin>>b;
+  if (a<b)
+  {
+   cout <<"A is the smallest number"; 
+  }
+  else
+  {
+    cout <<" B is the smallest number";
+  }
+   return 0;
+}
