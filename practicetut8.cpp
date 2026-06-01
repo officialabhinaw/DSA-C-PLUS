ac0a3185f3ed50e52cfbc23da25ffb861727b50c
Abhinaw kumar <>
2026-06-01 22:13:35 +0530

    add practice files

---
+++ practicetut8.cpp
@@ -0,0 +1,21 @@
+// max of two number
+// not for equal number
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter number a ";
+    cin>>a;
+     cout<<"Enter number b ";
+    cin>>b;
+    if(a>b)
+    {
+        cout<<"A is the largest  number ";
+    }
+    else
+    {
+        cout<<"B is the largest  number ";
+    }
+    return 0;
+}
