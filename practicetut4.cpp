2d3657017e5b6c064f0d5c32b50eb646946be021
Abhinaw kumar <>
2026-06-01 15:25:14 +0530

    add practice files

---
+++ practicetut4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int a;
+    cout<<"Enter a number";
+    cin>>a;
+    if(a%2==0)
+    {
+    cout<<"it is a even number";
+    }
+    else
+    {
+    cout<<"It is odd number";
+    }
+    return 0;
+}
