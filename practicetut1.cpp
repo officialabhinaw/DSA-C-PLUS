fa2bd77c2af669c5ab63833397d26bd85210d17a
Abhinaw kumar <>
2026-06-01 14:35:46 +0530

    add gitignore

---
+++ practicetut1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+cout<<"Enter a two number a ";
+cin >> a;
+cout<<"Enter a two number b ";
+cin >> b;
+sum=a+b;
+cout<< "The sum of number a & b ="  << sum << endl;
+return 0;
+}
--- tut1.cpp
+++ tut1.cpp
@@ -1,4 +1,4 @@
-// Your First C++ Program
+
 
 #include <iostream>
 
