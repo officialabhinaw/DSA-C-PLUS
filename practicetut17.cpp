eb831b75e34a47c81a6bdaabb11ad6330b10169b
Abhinaw kumar <>
2026-06-03 09:57:07 +0530

    add practice files

---
+++ practicetut17.cpp
@@ -0,0 +1,11 @@
+// teronary statment problem
+#include<iostream>
+using namespace std;
+ int main()
+ {
+   int n;
+   cout<<"Enter a number n=";
+   cin>>n;
+   cout<<(n>=0 ?"positive":"negative")<<endl;
+   return 0;
+ }
