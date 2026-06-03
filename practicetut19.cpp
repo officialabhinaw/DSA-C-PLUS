7e9d6766081354582758b80df5189c85b903b4df
Abhinaw kumar <>
2026-06-03 10:29:31 +0530

    add practice files

---
+++ practicetut19.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1,n;
+    
+    cout<<"Enter a number n=";
+    cin>>n;
+    while(i<=n)
+    {
+        cout<<i<<"\n";
+        i++;
+    }
+    cout<<endl;
+    return 0;
+}
