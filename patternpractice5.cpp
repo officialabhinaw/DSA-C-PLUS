2211dc83c9d5e140a3e3af51f49ce90a95411c60
Abhinaw kumar <>
2026-06-04 21:41:54 +0530

    add practice files

---
+++ patternpractice5.cpp
@@ -0,0 +1,23 @@
+//*
+//* *
+//* * *
+//* * * *
+// for this type of patterns
+#include<iostream>
+using namespace std;
+  int main()
+{
+ int n;
+ cout<<"Enter a number n=";
+ cin>>n;
+
+ for(int i=1;i<=n;i++)
+ {
+     for(int j=1;j<=i;j++)
+     {
+         cout<<"*"<<" ";
+     }
+    cout<<"\n"; 
+ }
+ return 0;
+}
