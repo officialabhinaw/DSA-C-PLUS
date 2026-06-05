677e261afa387e53f5fbff49b55dc1edf6536e18
Abhinaw kumar <>
2026-06-05 13:14:37 +0530

    add practice files

---
+++ patternpractice13.cpp
@@ -0,0 +1,36 @@
+//Enter a number n= 8
+//       *
+//      ***
+//     *****
+//    *******
+//   *********
+//  ***********
+// *************
+//***************
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int i,j;
+    cout<<"Enter a number n= ";
+    cin>>n;
+   for(int i=0;i<n;i++)
+   {
+       for(int j=0;j<n-i-1;j++)
+       {
+           cout<<" ";
+       }
+       for(int j=1;j<=i+1;j++)
+       {
+           cout<<"*";
+       }
+       for(int j=i;j>0;j--)
+       {
+           cout<<"*"; 
+       }
+       cout<<endl;
+   }
+   return 0;
+}
