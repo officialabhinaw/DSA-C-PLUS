7639ee9d032492d4555b8615d9e4b28a94a6205b
Abhinaw kumar <>
2026-06-05 13:50:46 +0530

    add practice files

---
+++ patternpractice14.cpp
@@ -0,0 +1,58 @@
+// hollow pyramidal
+//Enter a number n= 4
+/*   *
+  * *
+ *   *
+*     *
+ *   *
+  * *
+   *
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int i,j;
+    cout<<"Enter a number n= ";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout<<" ";
+        }
+        cout<<"*";
+        if(i!=0)
+        {
+        for(j=0;j<2*i-1;j++)
+        {
+          cout<<" ";  
+        }
+       cout<<"*";
+        }
+        cout<<endl;
+        
+    }
+     for(int i=n-2;i>=0;i--)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout<<" ";
+        }
+        cout<<"*";
+        if(i!=0)
+        {
+        for(j=0;j<2*i-1;j++)
+        {
+          cout<<" ";  
+        }
+       cout<<"*";
+        }
+        cout<<endl;
+        
+    }
+    
+    return 0;
+
+}
